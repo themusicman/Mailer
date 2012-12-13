6f29582abef3a99b309d16fa6c5eac449de56f04
kevin <kai.yuan@foxmail.com>
2012-12-14 01:29:00 +0800

    delete duplicate file

--- classes/mailer/transport/native.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-defined('SYSPATH') or die('No direct script access.');
-
-class Mailer_Transport_Native extends Kohana_Mailer_Transport_Native {
-    
-}
--- classes/mailer/transport/smtp.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-defined('SYSPATH') or die('No direct script access.');
-
-class Mailer_Transport_SMTP extends Kohana_Mailer_Transport_SMTP {
-    
-}
